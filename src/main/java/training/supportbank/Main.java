d9217a9cb62dd109a74b2dc5f28e084885c703aa
tbrough <tbrough54@gmail.com>
2021-09-20 19:51:17 +0100

    Main method created and reads out the Transactions

--- src/main/java/training/supportbank/Main.java
+++ src/main/java/training/supportbank/Main.java
@@ -1,8 +1,56 @@
 package training.supportbank;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
 public class Main {
+
+
+
     public static void main(String args[]) {
         // Your code here!
-        System.out.println("Test!");
+    //    System.out.println("Test!");
+//        Scanner scanne = new Scanner()
+        readFile();
+
+
+
+    }
+    public static void readFile(){
+     //   Path filePath = Paths.get("Transactions/Transactions2014.csv");
+     //   List<String> reader = null;
+        BufferedReader reader;
+        try {
+             reader = new BufferedReader(new FileReader("Transactions/Transactions2014.csv"));
+             String line = reader.readLine();
+             while (line != null){
+                 System.out.println(line);
+                 line = reader.readLine();
+             }
+             reader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+       // System.out.println(reader);
+    }
+
+}
+class Transactions {
+    public static void Date(){
+
+    }
+    public static void To(){
+
+    }
+    public static void From(){
+
+    }
+    public static void Amount(){
+
     }
 }
