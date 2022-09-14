1b4884bf382fa758a4cd6ffdc11a164d5358403c
imortimore <isaac.mortimore@internet.com>
2022-09-14 15:53:37 +0100

    csv added commit

--- src/main/java/training/supportbank/Account.java
+++ src/main/java/training/supportbank/Account.java
@@ -1,4 +1,11 @@
 package training.supportbank;
 
 public class Account {
+    //elements
+    protected String name;
+    protected float balance;
+    //constructor
+    public account (String name, float balance){
+        this.name = name;
+        this.balance = balance;
 }
--- src/main/java/training/supportbank/Main.java
+++ src/main/java/training/supportbank/Main.java
@@ -1,11 +1,32 @@
 package training.supportbank;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.io.BufferedReader;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String args[]) {
         // Your code here!
         System.out.println("Test!");
+        //variables
+        ArrayList<Account> Accounts = new ArrayList<Account>();
+        ArrayList<Transaction> transactions = new ArrayList<Transaction>();
+        //Load CSV
+        String line = "";
+        boolean firstskipped = false;
+        try{
+            BufferedReader reader = new BufferedReader(new FileReader("Transactions2014.csv"));
+            while ((line = reader.readLine()) != null){
+                if (firstskipped){
+                    String[] lineinput = line.split(",");
+                }else {
+                    firstskipped = true;
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
-
-
-
 }
